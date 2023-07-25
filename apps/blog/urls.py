7e4a171fb9df94d4e383d79ded960f08a6b1b45a
SARVAR RAHMONOV <tturkbola@gmail.com>
2023-07-25 16:44:06 +0500

    models.py added user field in requiredfieldsmodel

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework import routers
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.urls import path
+from .views import LoginView, RegisterView
 
 router = routers.DefaultRouter()
 
@@ -25,4 +27,8 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('api/register/', RegisterView.as_view(), name='register'),
 ]
+
+
