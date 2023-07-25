16f66e7f0dbde19066d2a59be667e7e6c7753cb9
SARVAR RAHMONOV <tturkbola@gmail.com>
2023-07-25 23:07:49 +0500

    BlogPostViewset added

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -4,7 +4,9 @@ from rest_framework.authtoken.models import Token
 from rest_framework import status
 from django.contrib.auth import authenticate
 from .serializers import UserSerializer
-
+from rest_framework import viewsets
+from .models import BlogPostModel, ReviewModel, CategoryModel
+from .serializers import BlogPostSerializer, ReviewModelSerializer, CategoryModelSerializer, TagModelSerializer
 
 class RegisterView(APIView):
     def post(self, request):
@@ -16,6 +18,7 @@ class RegisterView(APIView):
 
 
 class LoginView(APIView):
+
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
@@ -28,3 +31,23 @@ class LoginView(APIView):
             return Response(
                 {"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
             )
+
+
+class BlogPostViewSet(viewsets.ModelViewSet):
+    queryset = BlogPostModel.objects.all()
+    serializer_class = BlogPostSerializer
+
+
+    def list(self, request, *args, **kwargs):
+        most_recent_posts = self.queryset.order_by('-date_time')
+        most_targetted_posts = ReviewModel.objects.all().order_by('-review_score')[:2]
+        categories = CategoryModel.objects.all()
+
+        post = self.get_serializer(most_recent_posts, many=True)
+        targetted_posts = ReviewModelSerializer(most_targetted_posts, many=True)
+        category_counts = CategoryModelSerializer(categories, many=True)
+
+        return Response({'targetted_posts':targetted_posts.data,
+                         'category_counts':category_counts.data[0]["Categories count"],
+                         'post':post.data})
+
